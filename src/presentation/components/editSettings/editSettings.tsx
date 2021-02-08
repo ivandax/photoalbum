5832e30395919f0f325d17230d359598166687fd
ivandax <ivandax_89@hotmail.com>
2021-02-08 20:24:13 +0100

    improved the process of updating user with loader screen

--- src/presentation/components/editSettings/editSettings.tsx
+++ src/presentation/components/editSettings/editSettings.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { updateUser, UserWithId } from '../../../persistence/users';
+import { updateUser, UserWithId, getUser } from '../../../persistence/users';
+import {
+    setOngoingSessionData,
+    setValidSessionData,
+} from '../../../redux/sessionReducer';
 
 //components
 import FormInput from '../formInput';
@@ -12,27 +17,40 @@ interface EditSettingsProps {
 }
 
 const EditSettings = (props: EditSettingsProps): JSX.Element => {
+    const dispatch = useDispatch();
     const { userData } = props;
 
-    const [userChange, setUserChange] = useState({ name: '' });
+    const [userChange, setUserChange] = useState({ name: userData.name });
     const [message, setMessage] = useState('');
 
     const handleSave = async (event: React.FormEvent) => {
         event.preventDefault();
-        if (userChange.name.length === 0) {
+        if (userChange.name === userData.name) {
             setMessage('No se han detectado cambios.');
         } else {
+            dispatch(setOngoingSessionData());
             await updateUser(userChange, userData.id);
+            const updatedUser = await getUser(userData.id);
+            if (updatedUser === null || updatedUser === undefined) {
+                dispatch(setValidSessionData(userData));
+            } else {
+                dispatch(setValidSessionData(updatedUser));
+            }
         }
     };
 
     return (
         <form onSubmit={handleSave} className="editSettings">
             <h3>Editar información de perfil</h3>
+            <h5>Nombre:</h5>
             <div>
                 <FormInput
                     placeholder="Nombre"
-                    value={userChange.name !== '' ? userChange.name : userData.name}
+                    value={
+                        userChange.name === userData.name
+                            ? userData.name
+                            : userChange.name
+                    }
                     type="text"
                     onChange={(value) => setUserChange({ ...userChange, name: value })}
                 />
